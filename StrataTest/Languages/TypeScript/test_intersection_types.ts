576a99345cbdf6105c534d68122697da0420e70d
zhiyuuuu <b09705038@ntu.edu.tw>
2025-11-16 15:52:38 -0800

    [test] try on TSTypeLiteral (Value mismatch)

---
+++ StrataTest/Languages/TypeScript/test_intersection_types.ts
@@ -0,0 +1,12 @@
+type A = { name: string };
+const a: A = {
+    name: "Julia",
+};
+// type B = { age: number };
+
+// type Person = A & B;
+
+// const p: Person = {
+//     name: "Julia",
+//     age: 23,
+// };
